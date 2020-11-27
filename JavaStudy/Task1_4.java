ae2ba7cf601e84095c118306f628c7dd1b07f39e
watanabekanto <72913880+watanabekanto@users.noreply.github.com>
2020-11-27 17:27:37 +0900

    Update Task1_4.java

--- JavaStudy/Task1_4.java
+++ JavaStudy/Task1_4.java
@@ -27,7 +27,7 @@ String password = "pass";
     //   final修飾子を付与すると、変数（動的に変わる値）ではなく、定数（変更不可な定まった値）となります。
     //   「ここへ記述」部分へ適当な値を記述しましょう。
     //   ※詳しくはJava2章にて紹介します。
-
+    
     public static void main(String[] args) {
         // 以下の変数「name（名前）」「pass（パスワード）」を使用して、②〜⑤の条件を満たす処理を記述してください。
         // 名前とパスワードは画面からの入力値だと想定してみましょう。
@@ -35,22 +35,22 @@ String password = "pass";
          String pass = "pass";
         // ② 名前が「 alice 」、パスワードが「 pass 」であった場合、「 ログイン成功です 」と出力してください。
 if(name.equals("alice")&& pass.equals("pass")){
-    System.out.println("ログイン成功です");
+    System.out.println(CONST_MSG_SUCCESS);
 }
 
         // ③ 名前のみ正しい場合、「 パスワードに誤りがあります。 」と出力してください。
 else if(name.equals("alice")){
-    System.out.println("パスワードに誤りがあります。");
+    System.out.println(CONST_MSG_ERROR_PASS);
 }
 
         // ④ パスワードのみ正しい場合、「 名前に誤りがあります。 」と出力してください。
 else if(pass.equals("pass")){
-    System.out.println("名前に誤りがあります。");
+    System.out.println(CONST_MSG_ERROR_NAME);
 }
 
         // ⑤ 名前もパスワードも間違っていた場合は、「 入力情報に誤りがあります。 」と出力してください。
 else {
-    System.out.println("入力情報に誤りがあります");
+    System.out.println(CONST_MSG_ERROR_INPUT);
 }
     }
 
