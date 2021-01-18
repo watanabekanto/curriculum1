eebb9c8951d262fc82dbf5b64e30e9a32084bd07
watanabekanto <72913880+watanabekanto@users.noreply.github.com>
2021-01-18 16:37:14 +0900

    Update Task.java

--- JavaStudy/2-3/src/study/Task.java
+++ JavaStudy/2-3/src/study/Task.java
@@ -1,5 +1,7 @@
 package study;
 
+
+
 // ① TaskクラスにCalculatorクラスを継承させなさい。
 	public class Task extends Calculator{
     /**
@@ -9,8 +11,10 @@ package study;
 
         // ② Calculator.javaのすべてのオーバーロードメソッド「plus」に適当な引数を与え、下記画像のよう出力されるようコーディングしなさい。
         // 尚、「どのクラスから呼び出しているか」を明確にするため、plus()には呼び出し元のキーワードを付与すること。
-    	System.out.println("plusメゾットの引数が一つの場合:" + plus(10));
-    	System.out.println("plusメゾットの引数が一つの場合:" + plus(10,20));
-    	System.out.println("plusメゾットの引数が一つの場合:" + plus(10,20,30));
+    	super.plus(10,20,30);
+    	System.out.println("plusメゾットの引数が一つの場合:" + Calculator.plus(0));
+    	System.out.println("plusメゾットの引数が一つの場合:" + Calculator.plus(0,0));
+    	System.out.println("plusメゾットの引数が一つの場合:" + Calculator.plus(0,0,0));
+
     }
 }
